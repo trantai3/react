f88f26bd32923ff924d92a502563f326992f4b92
tai <trantai09032003@gmail.com>
2025-05-20 16:25:37 +0700

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import eslintPlugin from "@nabla/vite-plugin-eslint";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), eslintPlugin()],
 });
